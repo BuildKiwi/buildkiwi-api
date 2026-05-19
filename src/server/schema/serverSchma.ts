58acf8a29e433784dc9c64df5fd9f2166d664524
aditya1010-max <rauta2813@gmail.com>
2026-05-19 23:59:07 +0530

    added a schema for backend validation

---
+++ src/server/schema/serverSchma.ts
@@ -0,0 +1,14 @@
+export const projectSchema = {
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "description",
+      type: "textarea",
+      required: true,
+    },
+  ],
+};
